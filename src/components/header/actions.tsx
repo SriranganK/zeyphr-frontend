4397f94457ba9057819a002cee2450bc5171d1b1
its-me-sv <surajcpp115@gmail.com>
2025-05-04 16:52:39 +0530

    showing user info at actions

--- src/components/header/actions.tsx
+++ src/components/header/actions.tsx
@@ -3,6 +3,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
@@ -19,13 +20,39 @@ import {
 } from "../ui/drawer";
 import { Button } from "../ui/button";
 import HeaderBalance from "./balance";
+import { useEffect, useState } from "react";
+import { UserInfo } from "@/lib/types";
+import { useAppContext } from "@/context/app";
+import axios from "axios";
+import { Skeleton } from "../ui/skeleton";
 
 const AvatarActions: React.FC<AvatarActionsProps> = ({
   setShowLogout,
   setShowManageCard,
   publicKey,
 }) => {
+  const { token } = useAppContext();
   const isMobile = useIsMobile();
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [fetching, setFetching] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        setFetching(true);
+        const { data } = await axios.get<UserInfo>(
+          `/users/fetch-user?query=${publicKey}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        setUserInfo(data);
+      } finally {
+        setFetching(false);
+      }
+    };
+    fetchUser();
+  }, [publicKey, token]);
 
   const Wrapper = {
     Root: isMobile ? Drawer : DropdownMenu,
@@ -45,13 +72,30 @@ const AvatarActions: React.FC<AvatarActionsProps> = ({
         </Wrapper.Trigger>
       </ToolTip>
       <Wrapper.Content align="end" className="pb-2 sm:pb-1">
+        {fetching ? <div className="border-b py-1 mb-1 hidden sm:flex flex-col items-start space-y-0.5">
+          <Skeleton className="h-5 w-56" />
+          <Skeleton className="h-4 w-28" />
+        </div> : userInfo !== null && (
+          <>
+            <div className="px-2 py-1 hidden sm:flex sm:flex-col">
+              <p className="truncate font-medium">@{userInfo.username}</p>
+              <p className="text-xs text-muted-foreground">{userInfo.emailAddress}</p>
+            </div>
+            <DropdownMenuSeparator className="hidden sm:flex" />
+          </>)}
         {isMobile && (
           <>
             <DrawerHeader className="pb-0">
               <DrawerTitle>Actions</DrawerTitle>
               <DrawerDescription />
               <div className="flex items-center justify-between">
-                <p>Balance</p>
+                {fetching ? <div className="flex flex-col items-start space-y-0.5">
+                  <Skeleton className="h-4 w-32" />
+                  <Skeleton className="h-3 w-16" />
+                </div> : userInfo !== null && <div className="flex flex-col items-start">
+                  <p className="text-xs">@{userInfo.username}</p>
+                  <p className="text-xs text-muted-foreground">{userInfo.emailAddress}</p>
+                </div>}
                 <HeaderBalance />
               </div>
             </DrawerHeader>
